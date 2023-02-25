4f42861d9764627230558d05dab53cecce33c791
kedzkiest <kaede12kazu22@gmail.com>
2023-02-25 14:41:57 +0900

    再生ボタンでクリック音が流れ、停止ボタンでそれを停止できる機能を追加

--- Rhythm_Practice/Assets/Scripts/RhythmManager.cs
+++ Rhythm_Practice/Assets/Scripts/RhythmManager.cs
@@ -1,38 +1,68 @@
-using System.Collections;
+/* RhythmManager.cs
+ * 
+ * BPMやゲイン、拍などを変更しリズムを管理するためのスクリプトです
+ * 
+ * RhythmManagerゲームオブジェクトにアタッチして使います
+ * 
+ */
+
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class RhythmManager : MonoBehaviour
 {
-    [SerializeField] private AudioSource _ring;
-    [SerializeField] private double _bpm = 140d;
+    public double bpm = 140.0F;
+    public float gain = 0.5F;
+    public int signatureHi = 4;
+    public int signatureLo = 4;
+    private double nextTick = 0.0F;
+    private float amp = 0.0F;
+    private float phase = 0.0F;
+    private double sampleRate = 0.0F;
+    private int accent;
+    private bool running = false;
 
-    private double _metronomeStartDspTime;
-    private double _buffer = 2 / 60d;
-
-    private int cnt = 0;
-    private bool isRingOnce;
-
-    private void Start()
+    private void OnEnable()
     {
-        _metronomeStartDspTime = AudioSettings.dspTime;
+        accent = signatureHi;
+        double startTick = AudioSettings.dspTime;
+        sampleRate = AudioSettings.outputSampleRate;
+        nextTick = startTick * sampleRate;
+        running = true;
     }
 
-    private void FixedUpdate()
+    private void OnAudioFilterRead(float[] data, int channels)
     {
-        double nxtRng = NextRingTime();
+        if (!running)
+            return;
 
-        if (nxtRng < AudioSettings.dspTime + _buffer)
+        double samplesPerTick = sampleRate * 60.0F / bpm * 4.0F / signatureLo;
+        double sample = AudioSettings.dspTime * sampleRate;
+        int dataLen = data.Length / channels;
+        int n = 0;
+        while (n < dataLen)
         {
-            _ring.PlayScheduled(nxtRng);
+            float x = gain * amp * Mathf.Sin(phase);
+            int i = 0;
+            while (i < channels)
+            {
+                data[n * channels + i] += x;
+                i++;
+            }
+            while (sample + n >= nextTick)
+            {
+                nextTick += samplesPerTick;
+                amp = 1.0F;
+                if (++accent > signatureHi)
+                {
+                    accent = 1;
+                    amp *= 2.0F;
+                }
+                Debug.Log("Tick: " + accent + "/" + signatureHi);
+            }
+            phase += amp * 0.3F;
+            amp *= 0.993F;
+            n++;
         }
-    }
-
-    private double NextRingTime()
-    {
-        double beatInterval = 60d / _bpm;
-        double elapsedDspTime = AudioSettings.dspTime - _metronomeStartDspTime;
-        double beats = System.Math.Floor(elapsedDspTime / beatInterval);
-
-        return _metronomeStartDspTime + (beats + 1d) * beatInterval;
     }
 }
